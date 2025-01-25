dacd3ab38f6484714661d6cc596b7dd35fa888a4
avuidrauxs <audax338@gmail.com>
2025-01-25 01:19:11 +0100

    Add some error handling

--- src/api/simulate-exchange/controller.ts
+++ src/api/simulate-exchange/controller.ts
@@ -18,33 +18,37 @@ class SimulateExchangeController {
     }
 
     // POST /simulate-exchange
-    async simulateExchange(req: Request, res: Response) {
-        const { baseCurrency, targetCurrency, amount } = req.body as unknown as {
-            baseCurrency: string;
-            targetCurrency: string; 
-            amount: number;
-        };
-    
-        if (!baseCurrency || !targetCurrency || !amount) {
-            return res.status(400).json({ error: 'Missing required parameters' });
-        }
-
-        if (config.redis.enabled) {
-            const cacheKey = `exchange:${baseCurrency}:${targetCurrency}:${amount}`;
-            const cachedResult = await this.redisClient.get(cacheKey);
-      
-            if (cachedResult) {
-              const parsedResult = JSON.parse(cachedResult);
-              res.status(200).json({ ...parsedResult, cached: true });
-              return;
+    async simulateExchange(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { baseCurrency, targetCurrency, amount } = req.body as unknown as {
+                baseCurrency: string;
+                targetCurrency: string; 
+                amount: number;
+            };
+        
+            if (!baseCurrency || !targetCurrency || !amount) {
+                return res.status(400).json({ error: 'Missing required parameters' });
             }
-          }
 
-        const taskId = uuidv4();
-        const task = new SimulateExchangeTaskService(taskId, baseCurrency, targetCurrency, amount);
-        this.taskEngine.addTask(task);
-    
-        res.status(201).json({ taskId });
+            if (config.redis.enabled) {
+                const cacheKey = `exchange:${baseCurrency}:${targetCurrency}:${amount}`;
+                const cachedResult = await this.redisClient.get(cacheKey);
+          
+                if (cachedResult) {
+                  const parsedResult = JSON.parse(cachedResult);
+                  res.status(200).json({ ...parsedResult, cached: true });
+                  return;
+                }
+            }
+
+            const taskId = uuidv4();
+            const task = new SimulateExchangeTaskService(taskId, baseCurrency, targetCurrency, amount);
+            this.taskEngine.addTask(task);
+        
+            res.status(201).json({ taskId });
+        } catch (error) {
+            next(error);
+        }
     }
 
     private formatTaskResponse(task: any) {
@@ -64,8 +68,9 @@ class SimulateExchangeController {
     }
 
     // GET /simulate-exchange/:id
-    async getTaskStatus(req: Request, res: Response) {
-        const { taskId } = req.params;
+    async getTaskStatus(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { taskId } = req.params;
         const task = this.taskEngine.getTask(taskId);
         if (!task) {
             return res.status(404).json({ error: 'Task not found' });
@@ -73,15 +78,22 @@ class SimulateExchangeController {
         if (task.status === 'completed' && config.redis.enabled) {
             const cacheKey = `exchange:${task.payload.baseCurrency}:${task.payload.targetCurrency}:${task.payload.amount}`;
             await this.redisClient.set(cacheKey, JSON.stringify(task));
-          }
-        res.json(this.formatTaskResponse(task));
+            }
+            res.json(this.formatTaskResponse(task));
+        } catch (error) {
+            next(error);
+        }
     }
 
     // GET /simulate-exchange
-    async getAllTasks(req: Request, res: Response) {
-        const tasks = this.taskEngine.getAllTasks();
-        const tasksOverview = tasks.map(task => this.formatTaskResponse(task));
-        res.json(tasksOverview);
+    async getAllTasks(req: Request, res: Response, next: NextFunction) {
+        try {
+            const tasks = this.taskEngine.getAllTasks();
+            const tasksOverview = tasks.map(task => this.formatTaskResponse(task));
+            res.json(tasksOverview);
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
